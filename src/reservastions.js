4066e6710e4ef9d018cde68c7b094420a707c26a
Paul Adamski <pauladamski1985@gmail.com>
2026-01-26 20:00:27 +0100

    update time_09.1
    update typo

--- src/reservastions.js
+++ src/reservastions.js
@@ -12,14 +12,13 @@ cruiseType.addEventListener('change', () => {
 
   // Godzina nie dotyczy
   else if (
-    selected === 'Rejs o zachodzie słońca' ||
     selected === 'Impreza firmowa' ||
     selected === 'Ognisko' ||
     selected === 'Kajaki' ||
     selected === 'Kurs sternika motorowodnego'
   ) {
     cruiseTime.value = '08:00, 10:00, 12:00, 14:00, 16:00, 18:00';
-    cruiseTime.disabled = true;
+    cruiseTime.disabled = false;
   }
 
   // Reszta
