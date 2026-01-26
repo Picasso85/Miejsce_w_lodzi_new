c37d10f3c86fd6e74db15c27b98c12af2c594da4
Paul Adamski <pauladamski1985@gmail.com>
2026-01-26 19:38:36 +0100

    update_typo
    update typo

--- src/reservastions.js
+++ src/reservastions.js
@@ -4,16 +4,24 @@ const cruiseTime = document.getElementById('cruise-time');
 cruiseType.addEventListener('change', () => {
   const selected = cruiseType.value;
 
+  // Tylko 08:00
   if (selected === 'Tyniec' || selected === 'Kraków') {
-    // ustawienie godziny na 08:00 i zablokowanie pola
     cruiseTime.value = '08:00';
     cruiseTime.disabled = true;
-  } else if (selected === 'Kajaki' || selected === 'Kursy') {
-    // dla Kajaki i Kurs Sternika wyłączamy całkowicie wybór godziny
+  }
+
+  // Godzina nie dotyczy
+  else if (
+    selected === 'Ognisko' ||
+    selected === 'Kajaki' ||
+    selected === 'Kurs sternika motorowodnego'
+  ) {
     cruiseTime.value = '';
     cruiseTime.disabled = true;
-  } else {
-    // dla pozostałych opcji pole aktywne
+  }
+
+  // Reszta
+  else {
     cruiseTime.value = '';
     cruiseTime.disabled = false;
   }
